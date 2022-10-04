901258a8aca966a8c453d9f95b0cf7f5ea824384
Taneesha <taneeshanegi@gmail.com>
2022-10-03 23:19:59 -0700

    first try

--- q1.py
+++ q1.py
@@ -8,3 +8,19 @@
 # No need to use list
 # All input values are taken one by one separatively.
 ###
+val1 = int(input("Enter your first value: "))
+val2 = int(input("Enter your second value: "))
+val3 = int(input("Enter your third value: "))
+val4 = int(input("Enter your fourth value: "))
+val5 = int(input("Enter your fifth value: "))
+val6 = int(input("Enter your sixth value:"))
+val7 = int(input("Enter your seventh value: "))
+val8 = int(input("Enter your eigth value: "))
+val9= int(input("Enter your ninth value: "))
+val10 = int(input("Enter your tenth value: "))
+for  evennum in(val1, val2,val3,val4,val5,val6,val7,val8,val9,val10,):
+    if evennum % 2 == 0:
+        print (evennum)
+
+
+
